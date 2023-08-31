194d87e5a94951df02e84c734a7c28db9b1adb27
ridvikpal <pal.ridvik@gmail.com>
2023-08-31 11:46:55 -0400

    Added student class and setup GET request for API.

--- src/main/java/com/amigoscode/tutorial/TutorialApplication.java
+++ src/main/java/com/amigoscode/tutorial/TutorialApplication.java
@@ -2,14 +2,21 @@ package com.amigoscode.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
-//@RestController // tells us the TutorialApplication Class is a REST API Controller
+@RestController // tells us the TutorialApplication Class is a REST API Controller
 public class TutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
 	}
 
+	@GetMapping // for GET requests, return this from the server
+	public List<String> getRequest(){
+		return List.of("Hello", "World");
+	}
 }
---
+++ src/main/java/com/amigoscode/tutorial/student/Student.java
@@ -0,0 +1,83 @@
+package com.amigoscode.tutorial.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private Integer age;
+    private LocalDate dateOfBirth;
+    private String email;
+
+    public Student(){
+
+    }
+
+    // no ID constructor
+    public Student(String name, Integer age, LocalDate dateOfBirth, String email) {
+        this.name = name;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+    }
+
+    // all fields constructor
+    public Student(Long id, String name, Integer age, LocalDate dateOfBirth, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", dateOfBirth=" + dateOfBirth +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
